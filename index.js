7a51d7d19886a488f17e2729a04e0a229801adeb
le nghia <tro.ngnghia@yahoo.com.vn>
2017-06-15 07:52:19 +0000

    sss

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ var app = express();
 
 var storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, './uploads')
+    cb(null, __dirname+'./uploads')
   },
   filename: function (req, file, cb) {
     cb(null, file.originalname + '-' + Date.now())
